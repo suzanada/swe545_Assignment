0d80ef3befc7d1c6bf85c1ec3dc4f2dd51b50ec5
suzanada <sada@ku.edu.tr>
2016-04-28 23:32:59 +0300

    randomization fixed

--- assignment0Client.py
+++ assignment0Client.py
@@ -1,10 +1,18 @@
 import xmlrpclib
+from random import randint
+
 
 server = xmlrpclib.Server("http://www.advogato.org/XMLRPC")
-i = server.test.guess()
-server = xmlrpclib.Server("http://www.advogato.org/XMLRPC")
-b = server.test.guess()
-print "Random Number 1:",i[1]
-print "Random Number 2:",b[1]
-print "The sum of these 2 numbers is: ", server.test.sumprod(i[1],b[1])[0]
-print "The product of these 2 numbers is: ", server.test.sumprod(i[1],b[1])[1]
+
+def randomNumberGenerator():
+    return randint(0,100)
+
+ranNum1 = randomNumberGenerator()
+ranNum2 = randomNumberGenerator()
+
+print "Random Number 1:",str(ranNum1)
+print "Random Number 2:",str(ranNum2)
+
+
+print "The sum of Random Number 1 and Random Number 2: ", server.test.sumprod(ranNum1,ranNum2)[0]
+print "The product of Random Number 1 and Random Number 2: ", server.test.sumprod(ranNum1,ranNum2)[1]
